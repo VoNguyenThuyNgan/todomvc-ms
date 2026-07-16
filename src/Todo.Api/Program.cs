136918a2bbf0cdde80b8cbb113b1afe6f2fa416c
NaZoneYves <vngtngan@gmail.com>
2026-07-16 22:20:49 +0700

    feat(api): add MongoDB connection and create todo endpoint

--- src/Todo.Api/Program.cs
+++ src/Todo.Api/Program.cs
@@ -1,6 +1,18 @@
+using MongoDB.Driver;
+using MongoDB.Entities;
+using Todo.Api.Entities;
+
 var builder = WebApplication.CreateBuilder(args);
+
+await DB.InitAsync("TodoMVCDb", "localhost", 27017);
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+app.MapPost("/todos", async (TodoItem todo) =>
+{
+    await todo.SaveAsync();
+    return Results.Created($"/todos/{todo.ID}", todo);
+});
 
 app.Run();
