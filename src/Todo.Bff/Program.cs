db45c3564980e52944cdba9ccc1c6e115ed722c0
NaZoneYves <vngtngan@gmail.com>
2026-07-19 22:13:47 +0700

    feat(bff): update .http file, program, launch settings and app configuration

--- src/Todo.Bff/Program.cs
+++ src/Todo.Bff/Program.cs
@@ -1,6 +1,33 @@
+using Carter;
+using Todo.Bff.Clients;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Carter
+builder.Services.AddCarter();
+
+// Typed HttpClient
+builder.Services.AddHttpClient<ITodoApiClient, TodoApiClient>(client =>
+{
+    client.BaseAddress = new Uri(
+        builder.Configuration["TodoApi:BaseUrl"]!);
+});
+
+// CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("Angular", policy =>
+    {
+        policy.WithOrigins("http://localhost:4200")
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
 var app = builder.Build();
 
+app.UseCors("Angular");
+app.MapCarter();
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
