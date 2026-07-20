1b4454e7e019530c780bf1489d5aee73bf6ed2ed
NaZoneYves <vngtngan@gmail.com>
2026-07-20 17:21:45 +0700

    feat(bff): edit HttpResponseMessageExtensions file to ensure the response is correctly returned to the frontend

--- src/Todo.Bff/Extensions/HttpResponseMessageExtensions.cs
+++ src/Todo.Bff/Extensions/HttpResponseMessageExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Net;
+using System.Text;
 
 namespace Todo.Bff.Extensions
 {
@@ -6,13 +7,18 @@ namespace Todo.Bff.Extensions
     {
         public static async Task<IResult> ToResultAsync(this HttpResponseMessage response)
         {
+            if (response.StatusCode == HttpStatusCode.NoContent)
+            {
+                return Results.NoContent();
+            }
+
             var content = await response.Content.ReadAsStringAsync();
 
             var contentType =
                 response.Content.Headers.ContentType?.MediaType
                 ?? "application/json";
 
-            return Results.Content(
+            return Results.Text(
                 content,
                 contentType,
                 Encoding.UTF8,
